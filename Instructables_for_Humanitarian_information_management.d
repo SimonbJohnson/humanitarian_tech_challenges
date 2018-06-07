fe3b25b4ed35dba03c9aaf57027610554b7272d1
SimonbJohnson <simonbjohnson@gmail.com>
2018-06-07 16:54:02 +0100

    Create Instructables_for_Humanitarian_information_management.d

---
+++ Instructables_for_Humanitarian_information_management.d
@@ -0,0 +1,12 @@
+https://www.instructables.com/
+
+Small tutorials for information management.  Populate with HXL examples first
+
+1 How to use quickcharts
+2 How to use hxl proxy x8 for different filters
+3 How to make a chloropleth map
+4 How to use data check
+5 How to use tag with HXL
+6 Kobo to quickcharts
+7 kobo to datacheck
+8 kobo to google sheets
